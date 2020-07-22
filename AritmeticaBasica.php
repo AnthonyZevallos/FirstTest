b5d8a50c91eeb6e78a7f0b6b6a818ec745a105d4
robinvicente <robbin.vicente@gmail.com>
2020-07-21 21:27:45 -0500

    inicializar nuestro landing

---
+++ AritmeticaBasica.php
@@ -0,0 +1,17 @@
+<?php
+//Aritmética Básica con funciones
+
+    function suma($x, $y)
+    {
+        return $x + $y;
+    }
+
+    print(suma(12,7));
+    echo "<br>";
+
+    function resta($x, $y)
+    {
+        return $x - $y;
+    }
+
+    print(resta(12,7));
